e04cef00a48a03e7738fae2302e3a8f0f568b937
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-20 04:37:29 +0000

    Automatically backed up by Learn

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,16 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+ let looping = () =>{
+   let 
+   return movies.map((number) => `<div>${number.title} ${number.time}
+   ${number.genres.forEach(element => `<li>${element}</li>`)}
+   </div>`)
+  }
   return (
     <div>
-        {/*{code here}*/}
+       <h1>Movies Page</h1>
+       <div>{looping()}</div>
     </div>
   );
 };
